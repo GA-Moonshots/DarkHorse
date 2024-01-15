a9ecb4085ae3b85b192f3adf66c6f7cb73175d5d
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-01-15 14:37:05 -0500

    Fix ticks per inch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/OdometryConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/OdometryConfig.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.config;
 @com.acmerobotics.dashboard.config.Config
 public class OdometryConfig extends Config {
 
-    public static volatile double TICKS_PER_INCH = 2000  / (Math.PI * 4.8 * 2.54);
+    public static volatile double TICKS_PER_INCH = 2000 / (Math.PI * (4.8 / 2.54));
     public static volatile double LEFT_X_POSITION = -3.8d * TICKS_PER_INCH;
     public static volatile double RIGHT_X_POSITION = 3.8d * TICKS_PER_INCH;
     public static volatile double CENTER_Y_POSITION = 0.0d * TICKS_PER_INCH;
